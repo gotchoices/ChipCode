14a458aeb49629159cedd9c069b7378001ab5311
n8allan <3095770+n8allan@users.noreply.github.com>
2024-02-15 10:52:33 -0700

    Added getExpiration

--- src/code.ts
+++ src/code.ts
@@ -2,25 +2,30 @@ import crypto from 'crypto';
 import { CodeOptions as CodeOptions } from './code-options';
 import { calculateShannonEntropy, getSaltLength, frequencyTest, runsTest, arrayToBinaryString } from './code-validation';
 import { CryptoHash, arrayToBase64, base64ToArray } from 'chipcryptbase';
-import { interleveExpiration, isExpired } from '.';
+import { extractExpiration, interleveExpiration, isExpired } from '.';
 
 export class CryptoHashImpl implements CryptoHash {
 	constructor(public readonly options: CodeOptions) { }
-
 	/** Ensures that the given code (salt) passes all of the tests for high randomness */
 	isValid(code: string, now: number = Date.now()) {
-		const codeBin = base64ToArray(code);
-		return this.isValidArray(codeBin, now);
+		return this.isValidBin(base64ToArray(code), now);
 	}
 
-	/** Ensures that the given code (salt) passes all of the tests for high randomness */
-	isValidArray(code: Uint8Array, now: number = Date.now()) {
+	/** Ensures that the given code (salt) passes all of the tests for high randomness
+	 * Note: this is not part of the interface
+	*/
+	isValidBin(code: Uint8Array, now: number = Date.now()) {
 		const binary = arrayToBinaryString(code);
 		return !isExpired(code, now)
 			&& getSaltLength(code) >= this.options.byteLength
 			&& calculateShannonEntropy(binary) >= this.options.minEntropy
 			&& frequencyTest(binary) < this.options.frequencyPValueThreshold
 			&& runsTest(binary) > this.options.runsPValueThreshold;
+	}
+
+	/** @returns the expiration date/time (ms since Unix epoch) of the given code. */
+	getExpiration(code: string): number {
+		return extractExpiration(base64ToArray(code));
 	}
 
 	/** Generates a unique Code with sufficient entropy based on the given options.
@@ -36,7 +41,7 @@ export class CryptoHashImpl implements CryptoHash {
 			candidate = crypto.randomBytes(this.options.byteLength);
 			interleveExpiration(candidate, now + this.options.ageMs);
 			++tries;
-		} while (!this.isValidArray(candidate));
+		} while (!this.isValidBin(candidate));
 		return arrayToBase64(candidate);
 	}
 
